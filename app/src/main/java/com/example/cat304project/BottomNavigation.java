07cdc622e849fee9528f7d5e8d96d865577ad680
iffahazuwi <iffahazuwi@gmail.com>
2023-01-23 11:58:47 +0800

    add report button

---
+++ app/src/main/java/com/example/cat304project/BottomNavigation.java
@@ -0,0 +1,36 @@
+package com.example.cat304project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.cat304project.databinding.BottomNavigationBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigation extends AppCompatActivity {
+
+    private BottomNavigationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = BottomNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_booking, R.id.navigation_community, R.id.navigation_user)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_bottom_navigation);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/example/cat304project/ui/Community/CommunityFragment.java
@@ -0,0 +1,37 @@
+package com.example.cat304project.ui.Community;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.cat304project.databinding.FragmentCommunityBinding;
+
+public class CommunityFragment extends Fragment {
+
+    private FragmentCommunityBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CommunityViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(CommunityViewModel.class);
+
+        binding = FragmentCommunityBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textCommunity;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
