346d895b1d4037ac8d357eceb43525cc1343d2af
wannadzirah01 <wannadzirah.wa@gmail.com>
2023-01-27 11:26:03 +0800

    Updated create account and log in page. Firebase

--- app/src/main/java/com/example/cat304project/LoginActivity.java
+++ app/src/main/java/com/example/cat304project/LoginActivity.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText studentEmail, password;
-    Button btnLogIn;
-    DBHelper DB;
+    private EditText studentEmail, password;
+    private Button btnLogIn;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    //private Intent i;
 
+    //DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,28 +33,73 @@ public class LoginActivity extends AppCompatActivity {
         studentEmail = (EditText) findViewById(R.id.studentEmail1);
         password = (EditText) findViewById(R.id.password1);
         btnLogIn = (Button) findViewById(R.id.signIn1);
-        DB = new DBHelper(this);
-
+        //DB = new DBHelper(this);
+        //Intent i = new Intent(this, com.example.cat304project.LoginActivity.class);
+        //startActivity(i);
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //loginProgress.setVisibility(View.VISIBLE);
+                //btnLogIn.setVisibility(View.VISIBLE);
 
-                String user = studentEmail.getText().toString();
-                String pass = password.getText().toString();
+                final String email = studentEmail.getText().toString();
+                final String password1 = password.getText().toString();
+                mAuth = FirebaseAuth.getInstance();
 
-                if(user.equals("")||pass.equals(""))
-                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkstudentpass = DB.checkStudentEmailPassword(user, pass);
-                    if(checkstudentpass==true){
-                        Toast.makeText(LoginActivity.this, "You have successfully log in", Toast.LENGTH_SHORT).show();
-                        Intent intent  = new Intent(getApplicationContext(), BottomNavigation.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
-                    }
+                if (email.isEmpty() || password1.isEmpty()) {
+                    displayMessage("Please enter all field!");
+                    //btnLogIn.setVisibility(View.VISIBLE);
+                    //loginProgress.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    signIn(email,password1);
                 }
             }
         });
     }
+
+        private void signIn(String email, String password1) {
+            mAuth.signInWithEmailAndPassword(email,password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //loginProgress.setVisibility(View.INVISIBLE);
+                        //btnLogIn.setVisibility(View.VISIBLE);
+                        Intent i = new Intent(getApplicationContext(), BottomNavigation.class);
+                        startActivity(i);
+                        //updateUI();
+                    }
+                    else {
+                        displayMessage(task.getException().getMessage());
+                        //btnLogIn.setVisibility(View.VISIBLE);
+                        //loginProgress.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
+
+    //private void updateUI() {
+      //  startActivity(i);
+       // finish();
+    //}
+
+    private void displayMessage(String text) {
+        Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+    }
+
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null) {
+            //user is already connected  so we need to redirect him to home page
+            //updateUI();
+
+        }
+        else {
+
+        }
+    }*/
 }
--- app/src/main/java/com/example/cat304project/MainActivity.java
+++ app/src/main/java/com/example/cat304project/MainActivity.java
@@ -3,93 +3,232 @@ package com.example.cat304project;
 //import static android.os.Build.VERSION_CODES.R;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 //import com.example.cat304project.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText studentName, password, password2, studentEmail;
-    Button createAccount, logIn1;
-    DBHelper DB;
+    ImageView ImgUserPhoto;
+    static int PReqCode = 1 ;
+    static int REQUESCODE = 1 ;
+    Uri pickedImgUri ;
+
+    private EditText userName, userPassword, userPassword2, userEmail;
+    private Button createAccountBtn;
+    private ProgressBar loadingProgress;
+    private FirebaseAuth mAuth;
+    //DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        studentName = (EditText) findViewById(R.id.studentName);
-        studentEmail = (EditText) findViewById(R.id.studentEmail);
-        password = (EditText) findViewById(R.id.password);
-        password2 = (EditText) findViewById(R.id.confirmPassword);
-        createAccount = (Button) findViewById(R.id.createAccount);
-        logIn1 = (Button) findViewById(R.id.logIn1);
-        DB = new DBHelper(this);
+        userName = (EditText) findViewById(R.id.studentName);
+        userEmail = (EditText) findViewById(R.id.studentEmail);
+        userPassword = (EditText) findViewById(R.id.password);
+        userPassword2 = (EditText) findViewById(R.id.confirmPassword);
+        createAccountBtn = (Button) findViewById(R.id.createAccount);
+        Button logIn1Btn = (Button) findViewById(R.id.logIn1);
 
-        createAccount.setOnClickListener(new View.OnClickListener() {
+        //loadingProgress.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String userEmail = studentEmail.getText().toString();
-                String userName = studentName.getText().toString();
-                String pass = password.getText().toString();
-                String conPass = password2.getText().toString();
+                createAccountBtn.setVisibility(View.INVISIBLE);
+                //loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password1 = userPassword.getText().toString();
+                final String password2 = userPassword2.getText().toString();
+                final String name = userName.getText().toString();
 
-                if(userEmail.equals("")||userName.equals("")||pass.equals("")||conPass.equals(""))
-                    Toast.makeText(MainActivity.this, "Please enter all the fields!", Toast.LENGTH_SHORT).show();
-                else{
-                    if(pass.equals(conPass)){
-                        Boolean checkuser = DB.checkStudentEmail(userEmail);
-                        if(checkuser==false){
-                            Boolean insert = DB.insertData(userEmail, userName, pass);
-                            if(insert==true){
-                                Toast.makeText(MainActivity.this, "Account has been created successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), BottomNavigation.class);
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "User already exists! Please go to LOG IN page!", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(MainActivity.this, "Password does not matched!", Toast.LENGTH_SHORT).show();
-                    }
-                } }
-        });
-
-        logIn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-
+                if( email.isEmpty() || name.isEmpty() || password1.isEmpty()  || !password1.equals(password2)) {
+                    // display error message
+                    displayMessage("Please enter all fields!") ;
+                    createAccountBtn.setVisibility(View.VISIBLE);
+                    //loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    // CreateUserAccount method will try to create the user if the email is valid
+                    CreateUserAccount(email,name,password1);
+                }
             }
+
         });
+
+        /*logIn1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), BottomNavigation.class);
+                startActivity(i);
+            }
+        });*/
+
+        //ImgUserPhoto = findViewById(R.id.regUserPhoto) ;
+/*        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 22) {
+                    checkAndRequestForPermission();
+                }
+                else
+                {
+                    openGallery();
+                }
+            }
+        });*/
     }
 
-    public void launchHomePage(View v) {
+    private void CreateUserAccount(String email, final String name, String password) {
+        // this method create user account with specific email and password
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // user account created successfully
+                            displayMessage("Account has been successfully created!");
+                            Intent i  = new Intent(getApplicationContext(), BottomNavigation.class);
+                            startActivity(i);
+                            // after we created user account we need to update his profile picture and name
+                            //updateUserInfo( name ,pickedImgUri,mAuth.getCurrentUser());
+                        }
+                        else
+                        {
+                            // account creation failed
+                            displayMessage("Failed to create new account!" + task.getException().getMessage());
+                            createAccountBtn.setVisibility(View.VISIBLE);
+                            //loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+    }
+
+    // update user photo and name
+    /*private void updateUserInfo(final String name, Uri pickedImgUri, final FirebaseUser currentUser) {
+
+        // first we need to upload user photo to firebase storage and get url
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                // image uploaded succesfully
+                // now we can get our image url
+
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        // uri contain user image url
+
+                        UserProfileChangeRequest profleUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profleUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        if (task.isSuccessful()) {
+                                            // user info updated successfully
+                                            displayMessage("Register Complete");
+                                            updateUI();
+                                        }
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
+    }*/
+
+    private void updateUI() {
+        Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    // simple method to show toast message
+    private void displayMessage(String message) {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+
+    /*private void openGallery() {
+        //TODO: open gallery intent and wait for user to pick an image !
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }*/
+
+    /*private void checkAndRequestForPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(MainActivity.this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+        }
+        else
+            openGallery();
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null ) {
+            // the user has successfully picked an image
+            // we need to save its reference to a Uri variable
+            pickedImgUri = data.getData() ;
+            ImgUserPhoto.setImageURI(pickedImgUri);
+        };
+    }
+
+    /*public void launchHomePage(View v) {
         // launch home page activity
         Intent i = new Intent(this, BottomNavigation.class);
         startActivity(i);
-    }
+    }*/
 
-    public void launchLogin(View v) {
+    public void launchLogIn(View v) {
         // launch log in activity
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
+        finish();
     }
 
-    public void launchReport(View v){
-        //launch report activity
-        Intent i = new Intent(this, ReportActivity.class);
+    /*public void launchBottomNavigation(View v) {
+        // launch log in activity
+        Intent i = new Intent(this, BottomNavigation.class);
         startActivity(i);
-    }
+        finish();
+    }*/
 }
