e926e71245881389bd932d775fd7fb57316edd1b
iffahazuwi <iffahazuwi@gmail.com>
2023-01-30 00:56:17 +0800

    insert report details to database

--- app/src/main/java/com/example/cat304project/ReportActivity.java
+++ app/src/main/java/com/example/cat304project/ReportActivity.java
@@ -1,18 +1,46 @@
 package com.example.cat304project;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ReportActivity extends AppCompatActivity {
+
+    EditText reportDetails;
+    Button buttonSubmit;
+
+    DatabaseReference reportDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        reportDetails = findViewById(R.id.reportDetails);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        reportDB = FirebaseDatabase.getInstance().getReference().child("Reports");
+
+        buttonSubmit.setOnClickListener(view -> insertReportData());
+    }
+
+    private void insertReportData(){
+        String reportDescription = reportDetails.getText().toString();
+        Reports reports = new Reports(reportDescription);
+
+        if(reportDescription.isEmpty()){
+            Toast.makeText(this, "Report cannot be empty!", Toast.LENGTH_LONG).show();
+        }else{
+            reportDB.push().setValue(reports);
+            Toast.makeText(this, "Report has been submitted!", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void handleText(View v){
---
+++ app/src/main/java/com/example/cat304project/Reports.java
@@ -0,0 +1,13 @@
+package com.example.cat304project;
+
+public class Reports {
+    String reportData;
+
+    public Reports(String reportData){
+        this.reportData = reportData;
+    }
+
+    public String getReportData() {
+        return reportData;
+    }
+}
